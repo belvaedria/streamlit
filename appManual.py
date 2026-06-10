a3e2fba59adc6c92c675fae9ccd696a70f7c2144
“belvaedria” <belvaedriaz@gmail.com>
2026-06-10 10:08:21 +0700

    benerin  background part 9

--- appManual.py
+++ appManual.py
@@ -12,8 +12,8 @@ st.markdown("""
 .stApp {
     background-image:
         linear-gradient(
-            rgba(255,255,255,0.90),
-            rgba(255,255,255,0.90)
+            rgba(255,255,255,0.80),
+            rgba(255,255,255,0.80)
         ),
         url("https://raw.githubusercontent.com/belvaedria/streamlit/main/assets/apartment_bg.jpg");
         
